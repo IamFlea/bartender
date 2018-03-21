22f377c79d5dfcd57304a82f09759b1022291b2d
blk_panther <git.20.blk_panther@spamgourmet.com>
2018-03-21 14:44:14 +0100

    Add missing civilization string (Vikings)

--- aoc_civ.py
+++ aoc_civ.py
@@ -14,7 +14,7 @@ class Civilization(object):
     """
     # Check icons once agian
     icons = [3, 41, 46, 50, 45, 44, 36, 35, 43, 37, 39, 38, 42, 47, 106, 110, 108, 105, 117, 133, 93, 97, 99, 114, 190, 195, 197, 191, 231, 233, 230, 232, ]
-    strings = ["Gaia", "British", "French", "Goths", "Teutons", "Japanese", "Chinese", "Byzantine", "Persians", "Saracens", "Turks", "Mongols", "Celts", "Spanish", "Aztecs", "Mayan", "Huns", "Koreans", "Italians", "Indians", "Incas", "Magyars", "Slavs", "Portuguese", "Ethopians", "Malians", "Berbers", "Khmer", "Malay", "Burmese", "Vietnamese"]
+    strings = ["Gaia", "British", "French", "Goths", "Teutons", "Japanese", "Chinese", "Byzantine", "Persians", "Saracens", "Turks", "Vikings", "Mongols", "Celts", "Spanish", "Aztecs", "Mayan", "Huns", "Koreans", "Italians", "Indians", "Incas", "Magyars", "Slavs", "Portuguese", "Ethopians", "Malians", "Berbers", "Khmer", "Malay", "Burmese", "Vietnamese"]
     def __init__(self, owner):
         # ptr - pointer to a player
         ptr_civ_name = pm.pointer(owner.ptr + 0x1AE0)
